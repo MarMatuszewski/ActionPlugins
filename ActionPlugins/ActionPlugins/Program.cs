bde4aab5aee82bd60cc603b5ad0ce34facaa504f
Unknown <m.j.r.matuszewski@gmail.com>
2018-08-13 22:51:03 +0100

    Added Ninject NuGet and binded all plugins

--- ActionPlugins/ActionPlugins/Program.cs
+++ ActionPlugins/ActionPlugins/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using CorePlugin;
+using Ninject;
+using Ninject.Extensions.Conventions;
+using System;
+using System.IO;
+using System.Reflection;
 
 namespace ActionPlugins
 {
     class Program
     {
-        static void Main(string[] args)
+        private static IKernel _kernel;
+
+        static void Main( string[] args )
         {
-            Console.WriteLine("Hello World!");
+            _kernel = new StandardKernel();
+
+            bindAllCorePlugins();
+            bindAdditionalPlugins();
+        }
+
+        private static void bindAllCorePlugins()
+        {
+            _kernel.Bind( x => x
+                            .FromThisAssembly()
+                            .SelectAllClasses()
+                            .InheritedFrom<IPlugin>()
+                            .BindDefaultInterfaces()
+                            .Configure( y => y.InSingletonScope() ) );
+        }
+
+        private static void bindAdditionalPlugins()
+        {
+            var executableLocation = Assembly.GetEntryAssembly().Location;
+            var additionalPluginsPath = Path.Combine( Path.GetDirectoryName( executableLocation ), "Plugins" );
+
+            if( !Directory.Exists( additionalPluginsPath ) )
+            {
+                Directory.CreateDirectory( additionalPluginsPath );
+            }
+
+            _kernel.Bind( x => x
+                .FromAssembliesInPath( additionalPluginsPath )
+                .SelectAllClasses()
+                .InheritedFrom<IPlugin>()
+                .BindDefaultInterfaces()
+                .Configure( y => y.InTransientScope() ) );
         }
     }
 }
